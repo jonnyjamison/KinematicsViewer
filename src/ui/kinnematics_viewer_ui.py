e92ce657e921160ccdbea5031e6e0200e6e35f22
jonnyjamison <jonny.jamison@outlook.com>
2024-04-28 14:42:02 +0100

    Working plots from input table

--- src/ui/kinnematics_viewer_ui.py
+++ src/ui/kinnematics_viewer_ui.py
@@ -304,13 +304,3 @@ class KinematicsViewer(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.plotting_features = PlottingFeatures(self.ui)
 
-
-
-# if __name__ == "__main__":
-#     import sys
-#     app = QtWidgets.QApplication(sys.argv)
-#     MainWindow = QtWidgets.QMainWindow()
-#     ui = Ui_MainWindow()
-#     ui.setupUi(MainWindow)
-#     MainWindow.show()
-#     sys.exit(app.exec_())
--- src/ui/plotting_features.py
+++ src/ui/plotting_features.py
@@ -1,3 +1,4 @@
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QToolBar  # Import QToolBar from PyQt5.QtWidgets
 from PyQt5.QtWidgets import QTableWidgetItem, QVBoxLayout, QWidget
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -10,27 +11,58 @@ class PlottingFeatures:
         self.ui = ui
         self.setup_connections()
 
+
     def setup_connections(self):
         self.ui.plotButton.clicked.connect(self.handle_plot_button_click)
 
+
     def handle_plot_button_click(self):
-        upper_wishbone_values = self.get_upper_wishbone_values()
-        self.plot_upper_wishbone(upper_wishbone_values)
+        self.kinData_values = self.get_kinData_values()
+        self.plot_coordinates()
 
-    def get_upper_wishbone_values(self):
-        upper_wishbone_values = []
-        for column in range(self.ui.frontInput.columnCount()):
-            item = self.ui.frontInput.item(0, column)
-            if item is not None:
-                upper_wishbone_values.append(float(item.text()))  # Convert to float
-            else:
-                upper_wishbone_values.append(None)
-        return upper_wishbone_values
 
-    def plot_upper_wishbone(self, values):
-        x_values = values[::3]
-        y_values = values[1::3]
-        z_values = values[2::3]
+    def get_kinData_values(self):
+        
+        # Nested dictionary with 'front' and 'rear' nested
+        self.kinData_values = {}
+
+        # Access the tab widget containing the tables
+        tab_widget = self.ui.kinData
+
+        for position in ["front", "rear"]:
+            self.kinData_values[position] = {}
+            
+            # Access the correct table widget based on the position
+            table_widget_name = f"{position}Input"
+            table_widget = tab_widget.findChild(QtWidgets.QTableWidget, table_widget_name)
+            
+            for i, coordinate_name in enumerate(["upper_leading_pivot",
+                                                "upper_trailling_pivot",
+                                                "upper_upright_pivot",
+                                                "lower_leading_pivot",
+                                                "lower_trailling_pivot",
+                                                "lower_upright_pivot"]):
+                
+                self.kinData_values[position][coordinate_name] = []
+                
+                for column in range(3):
+                    # Accessing the item from the current table widget
+                    item = table_widget.item(i, column)
+                    if item is not None and item.text() != "":
+                        self.kinData_values[position][coordinate_name].append(float(item.text()))
+                    
+        return self.kinData_values
+                    
+    def plot_coordinates(self):
+
+        # Clear the plot area
+        layout = self.ui.plotArea.layout()
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget:
+                    widget.deleteLater()
 
         # Create a Matplotlib figure and canvas
         fig = Figure()
@@ -39,8 +71,15 @@ class PlottingFeatures:
         # Add a 3D subplot to the figure
         ax = fig.add_subplot(111, projection='3d')
 
-        # Plot the upper wishbone coordinates
-        ax.scatter(x_values, y_values, z_values, c='r', marker='o')
+        # Plot Origin
+        ax.scatter(0, 0, 0, c='r', marker='X')
+
+        # Plot the kinData coordinates
+        for position, values in self.kinData_values.items():
+            x_values = [value[0] for value in values.values()]
+            y_values = [value[1] for value in values.values()]
+            z_values = [value[2] for value in values.values()]
+            ax.scatter(x_values, y_values, z_values, label=position)
 
         # Hide grid lines and axis
         ax.grid(False)
@@ -56,4 +95,3 @@ class PlottingFeatures:
 
         # Update the plotArea widget
         self.ui.plotArea.setLayout(layout)
-
